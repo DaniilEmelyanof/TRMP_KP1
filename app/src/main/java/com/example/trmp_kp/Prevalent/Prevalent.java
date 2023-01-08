17308fb283e8d46a20c5b59143e325bc6d33dfed
DaniilEmelyanof <emelyanoff.daniil@gmail.com>
2023-01-08 17:46:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/trmp_kp/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.trmp_kp.Prevalent;
+
+import com.example.trmp_kp.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
